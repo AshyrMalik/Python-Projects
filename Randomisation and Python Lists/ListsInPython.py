e2d2f75b2972e2de1478676881dffdf6b33bfda4
Ashar Malik <malikashar898@gmail.com>
2025-01-20 23:23:03 +0500

    working with lists

---
+++ Randomisation and Python Lists/ListsInPython.py
@@ -0,0 +1,23 @@
+import random
+states_of_pakistan = [
+    "Punjab",
+    "Sindh",
+    "Khyber Pakhtunkhwa",
+    "Balochistan",
+    "Islamabad Capital Territory",
+    "Gilgit-Baltistan",
+    "Azad Jammu and Kashmir"
+]
+
+print(states_of_pakistan)
+
+states_of_pakistan[3]= "Balochi"
+print(states_of_pakistan)
+
+states_of_pakistan.append("India")
+print(states_of_pakistan)
+
+
+picker = random.randint(0,len(states_of_pakistan))
+
+print("the one who is picked is: ", {states_of_pakistan[picker]})
