dfdb2e6f77f282ce589e40d234f1be899f233a17
Ashar Malik <malikashar898@gmail.com>
2025-01-21 22:17:19 +0500

    Changed name

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,6 +1,27 @@
 import random
 
-word_list = ["ashar","malik","khan"]
+print("""
+  _                                            
+ | |                                           
+ | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __ 
+ | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \\
+ | | | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                     __/ |                     
+                    |___/                      
+""")
+
+
+word_list = ["python", "programming", "hangman", "developer", "computer",
+    "keyboard", "algorithm", "function", "variable", "syntax",
+    "loop", "condition", "integer", "boolean", "string",
+    "dictionary", "list", "tuple", "lambda", "exception",
+    "binary", "decimal", "hexadecimal", "recursive", "iteration",
+    "debugging", "compiler", "interpreter", "module", "package",
+    "virtual", "machine", "framework", "backend", "frontend",
+    "server", "database", "network", "protocol", "software",
+    "hardware", "pythonic", "array", "control", "structure",
+    "syntaxerror", "runtime", "bitwise", "hashing", "sorting"]
 hangman_stages = [
     """
       ------
@@ -85,14 +106,16 @@ while True:
     user_guess = input("Guess a letter: ").lower()
 
     found= False
+    n=0
     for i in chosen_word:
         if i==user_guess:
-            list_of_lines[count]=user_guess
+            list_of_lines[n]=user_guess
             print("right guess")
             print(list_of_lines)
             count+=1
             found=True
 
+        n+=1
     if not found:
         print("wrong guess")
         print(list_of_lines)
