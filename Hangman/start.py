cdc622b46740da0f91a70ce011632dfdb0f4f4b2
Ashar Malik <malikashar898@gmail.com>
2025-01-21 20:32:19 +0500

    modified logic

--- Hangman/start.py
+++ Hangman/start.py
@@ -6,7 +6,7 @@ chosen_word = random.choice(word_list)
 
 user_guess = input("Guess a letter: ")
 
-if chosen_word.contains(user_guess):
+if chosen_word.__contains__(user_guess):
     print("You have guessed the right letter")
 else:
     print("Wrong guess")
