b0f7f0ad967007fb2623094d9d0d97dcfa61487e
Ashar Malik <malikashar898@gmail.com>
2025-01-21 20:36:55 +0500

    modifying logic

--- Hangman/start.py
+++ Hangman/start.py
@@ -4,11 +4,14 @@ word_list = ["ashar","Malik","khan"]
 
 chosen_word = random.choice(word_list)
 
-user_guess = input("Guess a letter: ")
+# if chosen_word.__contains__(user_guess):
+#     print("You have guessed the right letter")
+# else:
+#     print("Wrong guess")
+for i in chosen_word:
+    if i==user_guess:
+        print("right")
+    else:
+        print("wrong")
 
-if chosen_word.__contains__(user_guess):
-    print("You have guessed the right letter")
-else:
-    print("Wrong guess")
-    
 
