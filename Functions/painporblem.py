a51dd6d4ce300fa9ed8efdb130e7bf929b93ea12
Ashar Malik <malikashar898@gmail.com>
2025-01-22 01:33:31 +0500

    minor changes

--- Functions/painporblem.py
+++ Functions/painporblem.py
@@ -1,8 +1,9 @@
 def paint_needed(height,width,coverage):
-    return round((height*width)/coverage)
+    paint_req= round((height*width)/coverage)
+    return paint_req
 
 
 height = int(input("What is the height of the wall: "))
-width = int(input("What is the width"))
-coverage= int(input("what is the coverage per can"))
-print("Pain required: ",paint_needed(height=height,width=width,coverage=coverage))
+width = int(input("What is the width: "))
+coverage= int(input("what is the coverage per can: "))
+print("Paint required: ",paint_needed(height=height,width=width,coverage=coverage))
