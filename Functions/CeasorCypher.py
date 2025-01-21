22cb8f429aec3a917238bce1eea5061b3c08550b
Ashar Malik <malikashar898@gmail.com>
2025-01-22 02:41:18 +0500

    added the encrypt message

--- Functions/CeasorCypher.py
+++ Functions/CeasorCypher.py
@@ -1,5 +1,13 @@
 
 alphabets=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def encrypt(text,shift):
+    encrypted_text=[]
+    for letter in text:
+        position=alphabets.index(letter)
+        new_position=shift+position
+        encrypted_text.append(alphabets[new_position])
+
+    return "".join(encrypted_text)
 
 direction=input("Type encode to encrypt the message \n"
       "Type decode to decrypt the message").lower()
