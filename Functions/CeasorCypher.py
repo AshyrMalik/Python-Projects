246a438fcc8bd17ffc54be884f890c7644df95bf
Ashar Malik <malikashar898@gmail.com>
2025-01-22 02:22:59 +0500

    wrote the structure

--- Functions/CeasorCypher.py
+++ Functions/CeasorCypher.py
@@ -1,2 +1,8 @@
 
 alphabets=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction=input("Type encode to encrypt the message \n"
+      "Type decode to decrypt the message").lower()
+
+print("Type youre message: ")
+shift=int(input("Enter the number which you want to shift: "))
