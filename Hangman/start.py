6de43d3c798f93b7815cbfec78745538e1841623
Ashar Malik <malikashar898@gmail.com>
2025-01-21 21:28:56 +0500

    adjusting the main loop

--- Hangman/start.py
+++ Hangman/start.py
@@ -1,26 +1,26 @@
 import random
 
-word_list = ["ashar","Malik","khan"]
+word_list = ["ashar","malik","khan"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
-user_guess = input("Guess a letter: ").lower()
 
 list_of_lines =[]
 for i in range(len(chosen_word)):
     list_of_lines.append("_")
+count=0
+print(list_of_lines)
 
-# if chosen_word.__contains__(user_guess):
-#     print("You have guessed the right letter")
-# else:
-#     print("Wrong guess")
 for i in chosen_word:
+    user_guess = input("Guess a letter: ").lower()
     if i==user_guess:
-        print("right")
         list_of_lines[count]=user_guess
+        print(list_of_lines)
         count+=1
     else:
         print("wrong")
         count+=1
 
+if count==len(chosen_word):
+    print("You have won")
 
