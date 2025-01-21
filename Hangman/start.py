a50ab1c72f65860ecf975642659287d2d539e7bf
Ashar Malik <malikashar898@gmail.com>
2025-01-21 21:36:42 +0500

    adjusting the main loop

--- Hangman/start.py
+++ Hangman/start.py
@@ -67,8 +67,7 @@ hangman_stages = [
     """
 ]
 
-# Example: Displaying the last stage (fully hanged)
-print(hangman_stages[-1])
+
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -76,18 +75,22 @@ print(chosen_word)
 list_of_lines =[]
 for i in range(len(chosen_word)):
     list_of_lines.append("_")
+
 count=0
 print(list_of_lines)
+lives = 6
 
 for i in chosen_word:
     user_guess = input("Guess a letter: ").lower()
+
     if i==user_guess:
         list_of_lines[count]=user_guess
         print(list_of_lines)
         count+=1
     else:
-        print("wrong")
+        lives-=1
         count+=1
+
 
 if count==len(chosen_word):
     print("You have won")
