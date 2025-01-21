a6a34e5f5ee3c41bea5b25b5352ecae3ecfd9178
Ashar Malik <malikashar898@gmail.com>
2025-01-21 16:01:38 +0500

    Created Password Generator.py

---
+++ Loops/Password Generator.py
@@ -0,0 +1,22 @@
+import random
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', '<', '>', ',', '.', '?', '/', '|', '\\', '~', '`']
+numbers_as_strings = [str(i) for i in range(10)]
+alphabets = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
+
+length= int(input("How long would you like your password to be: "))
+numbers=int(input("How many numbers would you like: "))
+sym=int(input("How many symbols would you like: "))
+
+password=""
+for i in range(1,sym+1):
+    password= password + symbols[random.randint(0,len(symbols))]
+
+for i in range(1,numbers+1):
+    password= password + numbers_as_strings[random.randint(0,len(numbers_as_strings))]
+
+for i in range(length-numbers-sym+1):
+    password= password + alphabets[random.randint(0,len(alphabets))]
+
+password_list = list(password)
+password= ''.join(random.shuffle(password_list))
+print("Youre password is : ", password)
