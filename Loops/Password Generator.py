65e4a0aead86076aaab14b8a0a9c8b79c0f8b0e2
Ashar Malik <malikashar898@gmail.com>
2025-01-21 16:09:57 +0500

    minor change

--- Loops/Password Generator.py
+++ Loops/Password Generator.py
@@ -3,9 +3,9 @@ symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+',
 numbers_as_strings = [str(i) for i in range(10)]
 alphabets = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
 
-length= int(input("How long would you like your password to be: "))
-numbers=int(input("How many numbers would you like: "))
-sym=int(input("How many symbols would you like: "))
+length = int(input("How long would you like your password to be: "))
+numbers = int(input("How many numbers would you like: "))
+sym = int(input("How many symbols would you like: "))
 
 password = ""
 for i in range(sym):
