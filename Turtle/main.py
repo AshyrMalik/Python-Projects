28aab30e25daeaa7756a5bd0cac2246ab3a173db
Ashar Malik <malikashar898@gmail.com>
2025-01-30 19:50:19 +0500

    working with event listner in turtle

--- Turtle/main.py
+++ Turtle/main.py
@@ -3,36 +3,22 @@ from turtle import Turtle,Screen
 timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("red")
+#
+#
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
 
-timmy.forward(100)
-timmy.left(90)
-timmy.forward(100)
-timmy.left(90)
-timmy.forward(100)
-timmy.left(90)
-timmy.forward(100)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def move_forward():
+    timmy.forward(100)
 
 
 
 
 screen = Screen()
+screen.listen()
+screen.onkey(key="space",fun= move_forward)
 screen.exitonclick()
-
