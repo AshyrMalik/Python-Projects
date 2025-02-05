6532a3b3fb490cbc4583bd530702597264a7c8aa
Ashar <malikashar898@gmail.com>
2025-02-05 15:11:58 +0500

    some minor changes

--- Turtle Road Crossing/car_manager.py
+++ Turtle Road Crossing/car_manager.py
@@ -5,9 +5,18 @@ MOVE_INCREMENT = 10
 
 class CarManager(Turtle):
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.penup()
-        self.color(random.choice(COLORS))
-        self.goto(300,random.randint(-300,300))
-        self.left(180)
+        self.all_cars = []
+
+    def create_car(self):
+        new_car = Turtle()
+        new_car.shape("square")
+        new_car.shapesize(stretch_wid=1, stretch_len=2)  # Cars should be wider than tall
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        new_car.goto(300, random.randint(-250, 250))  # Keep cars within visible range
+        new_car.setheading(180)  # Face left
+        self.all_cars.append(new_car)
+
+    def move(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)  # Move left
