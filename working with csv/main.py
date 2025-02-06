88e965fadb6de04e863a1c507ac12887927acb38
Ashar <malikashar898@gmail.com>
2025-02-06 15:25:24 +0500

    opening file with csv

---
+++ working with csv/main.py
@@ -0,0 +1,11 @@
+# import csv
+#
+# with open("weather_data.csv") as data_files:
+#     data = csv.reader(data_files)
+#     temp_list=[]
+#     for row in data:
+#         print(row)
+#         if row[1]!='temp':
+#             temp_list.append(row[1])
+#
+#     print(temp_list)
