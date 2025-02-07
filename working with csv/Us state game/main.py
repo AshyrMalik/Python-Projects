b3a99e24b3f7c086403de0a8dcc749ce178281eb
Ashar <malikashar898@gmail.com>
2025-02-07 23:43:03 +0500

    some final revisions

--- working with csv/Us state game/main.py
+++ working with csv/Us state game/main.py
@@ -1,30 +1,29 @@
 import turtle
-from turtle import Screen , Turtle
+from turtle import Screen, Turtle
 import pandas as pd
 
-
 screen = Screen()
-screen.title("U.S. State game")
+screen.title("U.S. State Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-# def get_mouse_click_coor(x, y):
-#     print(x, y)
-#
-# turtle.onscreenclick(get_mouse_click_coor)
-
+# Load data
 data = pd.read_csv("50_states.csv")
-states = data["states"]
-list_of_states =states.list().lower()
 
+# Convert states column to a lowercase list
+list_of_states = data["state"].str.lower().tolist()
+
+# Game loop
 is_on = True
-
+ # Initialize count
+guessed_list=[]
 while is_on:
-    user_guess = screen.textinput(title="Name a state", prompt="Enter another state name that you know  ")
+
+    user_guess = screen.textinput(title=f"{(len(guessed_list))}/50 correct states", prompt="Enter another state name that you know").lower()
 
     if user_guess in list_of_states:
+        guessed_list.append(user_guess)
         tim = Turtle()
         tim.penup()
         tim.hideturtle()
@@ -36,11 +35,8 @@ while is_on:
         tim.goto(int(guessed_state["x"].iloc[0]), int(guessed_state["y"].iloc[0]))
         tim.write(f"{user_guess.title()}", align="center", font=("Courier", 10, "normal"))
 
-        count = 0  # Reset count when a correct answer is given
+        continue
 
-    if count == 1:
-        is_on = False
+    is_on = False
+
 turtle.mainloop()
-pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-
-
