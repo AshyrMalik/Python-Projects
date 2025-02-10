8520c4b0dd60ef6b0242e94487a9f8ecfd6231e2
Ashar <malikashar898@gmail.com>
2025-02-10 21:59:35 +0500

    adding function in using positional arguments in place

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -1,13 +1,21 @@
 import tkinter
 
-window = tkinter.Tk()
-window.title("My first Gui")
-window.minsize(width=500, height= 300)
+# window = tkinter.Tk()
+# window.title("My first Gui")
+# window.minsize(width=500, height= 300)
+#
+# my_label = tkinter.Label(text="Hello world")
+# my_label.pack(side ="left")
+#
 
+def add(*args):
+    sum = 0
+    for num in args:
+        sum+=num
 
+    return sum
 
+print(add(5,5,5,5,5))
 
-
-
-
-window.mainloop()
+#
+# window.mainloop()
