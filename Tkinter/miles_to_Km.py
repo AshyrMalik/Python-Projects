594ccbc4dac41f922036d9b39b5c5e517c170add
Ashar <malikashar898@gmail.com>
2025-02-12 12:17:29 +0500

    added them into grid

--- Tkinter/miles_to_Km.py
+++ Tkinter/miles_to_Km.py
@@ -1,11 +1,26 @@
 from tkinter import *
 
+
 window=Tk()
 window.title("Mile to Km Converter")
-
+window.config(padx=20,pady=20)
 miles_input= Entry()
+miles_input.grid(column=1,row=0)
+
 miles_label = Label(text="Miles")
+miles_label.grid(column=2,row=0)
+
 is_equal_label=Label(text="is equal to")
+is_equal_label.grid(column=0,row=1)
+
 km_result_label= Label(text="0")
+km_result_label.grid(column=1,row=1)
+
 km_label = Label(text="km")
+km_label.grid(column=2,row=1)
+
 calculate_buttons =Button(text="Calculate")
+calculate_buttons.grid(column=1,row=3)
+
+
+window.mainloop()
