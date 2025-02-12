17709e9dbd7ec91f5ef7531daba33a3db9f39761
Ashar <malikashar898@gmail.com>
2025-02-12 12:04:59 +0500

    initializing labels

--- Tkinter/miles_to_Km.py
+++ Tkinter/miles_to_Km.py
@@ -1,2 +1,11 @@
 from tkinter import *
 
+window=Tk()
+window.title("Mile to Km Converter")
+
+miles_input= Entry()
+miles_label = Label(text="Miles")
+is_equal_label=Label(text="is equal to")
+km_result_label= Label(text="0")
+km_label = Label(text="km")
+calculate_buttons =Button(text="Calculate")
