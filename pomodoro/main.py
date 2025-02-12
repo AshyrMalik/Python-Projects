0112b5393a865da3fe97467f44d2099cabbc5969
Ashar <malikashar898@gmail.com>
2025-02-12 15:58:45 +0500

    added the text label

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -18,8 +18,12 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 windows = Tk()
 windows.title("Pomodoro")
-windows.config(padx=100,pady=50)
-canvas = Canvas(width=200,height=224)
+windows.config(padx=100,pady=50,bg=YELLOW)
+
+text_label=Label(text="Timer",fg=GREEN,bg=YELLOW,font=(FONT_NAME,50,"bold"))
+text_label.grid(row=0,column=1)
+
+canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(103,112,image=tomato)
 canvas.create_text(103,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
