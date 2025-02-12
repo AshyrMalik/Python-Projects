5c63c36b2189cfd14c5c621885952bec69e9ee1f
Ashar <malikashar898@gmail.com>
2025-02-12 15:25:12 +0500

    created canvas

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -16,3 +16,12 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+windows = Tk()
+windows.title("Pomodoro")
+
+canvas = Canvas(width=200,height=210)
+tomato = PhotoImage(file="tomato.png")
+canvas.create_image(100,112,image=tomato)
+canvas.pack()
+
+windows.mainloop()
