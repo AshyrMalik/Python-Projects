3067b4c3ea41aa483bea5b51eb84e1cc282e8229
Ashar <malikashar898@gmail.com>
2025-02-14 12:43:37 +0500

    added the image to canvas

--- password manager/main.py
+++ password manager/main.py
@@ -3,3 +3,17 @@
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+windows = Tk()
+windows.title("Password Manager")
+windows.config(padx=20,pady=20)
+
+
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file= "logo.png")
+canvas.create_image(100,100,image=logo)
+canvas.pack()
+
+
+
+windows.mainloop()
