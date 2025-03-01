d103df7f547ef2bfc62580fb85ee8135431f8910
Ashar <malikashar898@gmail.com>
2025-03-01 18:09:40 +0500

    Added the labels

--- password manager/main.py
+++ password manager/main.py
@@ -13,7 +13,18 @@ windows.config(padx=20,pady=20)
 canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file= "logo.png")
 canvas.create_image(100,100,image=logo)
-canvas.pack()
+canvas.grid(row=0,column=1)
+
+# Labels Column
+website_label = Label(text="Website")
+website_label.grid(row=1,column=0)
+email_label = Label(text="Email")
+email_label.grid(row=2,column=0)
+pass_label = Label(text="Password")
+pass_label.grid(row=3,column=0)
+
+
+
 
 
 
