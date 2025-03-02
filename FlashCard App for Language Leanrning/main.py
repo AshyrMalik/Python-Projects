a78a66dd8adb8d81e8a9cf7b5c5da5ed85c05581
Ashar <malikashar898@gmail.com>
2025-03-02 13:17:15 +0500

    added the buttons

--- FlashCard App for Language Leanrning/main.py
+++ FlashCard App for Language Leanrning/main.py
@@ -1,2 +1,22 @@
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+windows = Tk()
+windows.title("Flash Card App")
+windows.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
+#Buttons
+
+right_image = PhotoImage(file=r"C:\Users\HP\PycharmProjects\Python-Projects\FlashCard App for Language Leanrning\images\right.png")
+right_button = Button(image=right_image, highlightthickness=0,padx=50,pady=50 )
+right_button.grid(row=2, column=1)
+
+wrong_image = PhotoImage(file=r"C:\Users\HP\PycharmProjects\Python-Projects\FlashCard App for Language Leanrning\images\wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(row=2, column=6)
+
+
+
+windows.mainloop()
+
